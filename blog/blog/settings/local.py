40faa1702f72b53e26b6ee618aa44edaa8e1113b
micaela <micaelacalvente@gmail.com>
2023-11-23 17:40:13 -0300

    clase 4

--- blog/blog/settings/local.py
+++ blog/blog/settings/local.py
@@ -9,7 +9,11 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog_comision6',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
