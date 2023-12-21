6fad8d85fb9de0405845f1f18fbe6dc534249dee
micaela <micaelacalvente@gmail.com>
2023-12-21 16:06:57 -0300

    deploy

--- blog/blog/settings/base.py
+++ blog/blog/settings/base.py
@@ -15,6 +15,9 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+DEBUG = False
+
+ALLOWED_HOSTS = ['micacomision6.pythonanywhere.com']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
--- blog/blog/settings/prod.py
+++ blog/blog/settings/prod.py
@@ -1,8 +1,5 @@
 from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-ALLOWED_HOSTS = ['micacomision6.pythonanywhere.com']
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
