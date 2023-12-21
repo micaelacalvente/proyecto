46776384a40988a002d878e7f209028bd6b0ace7
micaela <micaelacalvente@gmail.com>
2023-12-21 16:28:29 -0300

    deploy

--- blog/blog/settings/local.py
+++ blog/blog/settings/local.py
@@ -1,8 +1,8 @@
 from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
-'''DEBUG = True
+DEBUG = True
 
-ALLOWED_HOSTS = []'''
+ALLOWED_HOSTS = []
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
