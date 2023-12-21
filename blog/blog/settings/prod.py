8f9150de1b3399ef56de98301e183825baead5ea
micaela <micaelacalvente@gmail.com>
2023-12-21 15:55:53 -0300

    deploy

---
+++ blog/blog/settings/prod.py
@@ -0,0 +1,19 @@
+from .base import *
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['micacomision6.pythonanywhere.com']
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'micacomision6$default',
+        'USER': 'micacomision6',
+        'PASSWORD': 'informatorio2023',
+        'HOST': 'micacomision6.mysql.pythonanywhere-services.com',
+        'PORT': '',
+    }
+}
